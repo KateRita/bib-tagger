85245e48a9f99a0808ff17728a7531d2a7b585e4
Kate Reading <katerita@gmail.com>
2016-04-18 23:34:47 -0700

    created main & module files

---
+++ bibtagger/bibtagger.py
@@ -0,0 +1,6 @@
+import cv2
+
+def findBibs(image):
+    print "Hello world"
+
+    return 1234
---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+import cv2
+
+import bibtagger.bibtagger as bt
+
+#read in photos
+
+#for each photo
+
+if __name__ == "__main__":
+
+    sourcefolder = os.path.abspath(os.path.join(os.curdir, 'photos'))
+    outfolder = os.path.abspath(os.path.join(os.curdir, 'photos-out'))
+
+    # Ensure that the directory that holds our output directories exists...
+    if not os.path.exists(outfolder):
+        os.mkdir(outfolder)
+
+    print 'Searching for image folders in {} folder'.format(sourcefolder)
+
+    # Extensions recognized by opencv
+    exts = ['.bmp', '.pbm', '.pgm', '.ppm', '.sr', '.ras', '.jpeg', '.jpg',
+            '.jpe', '.jp2', '.tiff', '.tif', '.png']
+
+  # For every image in the source directory
+    for racephoto_dir in os.listdir(sourcefolder):
+        print "Collecting images from directory {}".format(racephoto_dir)
+        img_list = []
+        img_namelist = []
+        filenames = sorted(os.listdir(os.path.join(sourcefolder, racephoto_dir)))
+
+        for filename in filenames:
+            name, ext = os.path.splitext(filename)
+            if ext in exts:
+                img_list.append(cv2.imread(os.path.join(sourcefolder, racephoto_dir, filename)))
+                img_namelist.append(filename)
+
+        print "Extracting bibs."
+        for idx,image in enumerate(img_list):
+            #Do Operation
+            out_list = bt.findBibs(image)
+            print img_namelist[idx] , ":", out_list
+
+        #make output directory
+        #print "writing output to {}".format(os.path.join(outfolder, racephoto_dir))
+        #if not os.path.exists(os.path.join(outfolder, racephoto_dir)):
+        #    os.mkdir(os.path.join(outfolder, racephoto_dir))
+
+        #write output image
+        #for idx, image in enumerate(out_list):
+        #    cv2.imwrite(os.path.join(outfolder, racephoto_dir, 'frame{0:04d}.png'.format(idx)), image)
