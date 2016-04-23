5e40c4bd335f83420cc1a9ee38f36ec6c5888b6e
Ryan Reading <ryanreading@Ryan-Reading-MBP-15.local>
2016-04-23 14:36:20 -0700

    find_bibs: Added find_bib method that works on one sample image

--- bibtagger/find_bibs.py
+++ bibtagger/find_bibs.py
@@ -1,11 +1,39 @@
 import cv
 import cv2
 
+#
+# Takes an input image which is assumed to only have one bib and returns the
+# four corners of the bib in the form of an opencv contour (vector of points,
+# i.e vector((x1,y1),(x2,y2),(x3,y3)(x4,y4)) )
+#
+def find_bib(image):
+  edges = cv2.Canny(image,175,200)
+  contours,hierarchy = find_contours(edges)
+  rectangles = get_rectangles(contours)
+
+  potential_bibs = [rect for rect in rectangles if is_potential_bib(rect)]
+
+  cv2.drawContours(image,potential_bibs,-1,(0,0,255), 2)
+  cv2.imwrite("with_potentials.jpg", image)
+
+  return potential_bibs[0]
+
+#
+# Checks that the size and aspect ratio of the contour is appropriate for a bib.
+#
+def is_potential_bib(rect):
+  x,y,w,h = cv2.boundingRect(rect)
+  aspect_ratio = float(w) / float(h)
+  print w,h,aspect_ratio
+  return (cv2.contourArea(rect) > 500 and
+          aspect_ratio > 1 and
+          aspect_ratio < 2)
+
 def find_bibs(image):
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY);
   binary = cv2.GaussianBlur(gray,(5,5),0)
-  #ret,binary = cv2.threshold(binary, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU);
-  binary = cv2.adaptiveThreshold(binary, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+  ret,binary = cv2.threshold(binary, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU);
+  #binary = cv2.adaptiveThreshold(binary, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
   #ret,binary = cv2.threshold(binary, 190, 255, cv2.THRESH_BINARY);
 
   #lapl = cv2.Laplacian(image,cv2.CV_64F)
@@ -16,7 +44,7 @@ def find_bibs(image):
 
   edges = cv2.Canny(image,175,200)
   cv2.imwrite("edges.jpg", edges)
-  #binary = edges
+  binary = edges
 
   cv2.imwrite("binary.jpg", binary)
   contours,hierarchy = find_contours(binary)
@@ -25,21 +53,20 @@ def find_bibs(image):
 
 def find_contours(image):
   #return cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE);
-  return cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE);
+  #return cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE);
+  return cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE);
 
-def get_corners(contours):
+def get_rectangles(contours):
   #remove_contours(contours, 100, 5000);
-  corners = []
+  rectangles = []
   for contour in contours:
     epsilon = 0.02*cv2.arcLength(contour,True)
-    approx = cv2.approxPolyDP(contour,epsilon,True)
-    #print len(contour),len(approx)
-    #if (len(approx) >= 4 and cv2.isContourConvex(approx)):
-      #sort_rect_corners(approx)
-      #corners.append(approx)
-    corners.append(approx)
+    hull = cv2.convexHull(contour)
+    approx = cv2.approxPolyDP(hull,epsilon,True)
+    if (len(approx) == 4 and cv2.isContourConvex(approx)):
+      rectangles.append(approx)
 
-  return corners
+  return rectangles
 
 def find_lines(img):
   edges = cv2.Canny(img,100,200)
@@ -72,24 +99,23 @@ def find_keypoints(img):
 if __name__ == "__main__":
   image_1 = cv2.imread("../photos/GloryDays/1.jpg")
 
-  sample = cv2.imread("../photos/GloryDays/bib-sample.jpg")
-  orb = cv2.ORB();
+  #sample = cv2.imread("../photos/GloryDays/bib-sample.jpg")
+  #orb = cv2.ORB();
 
-  find_lines(image_1)
+  #find_lines(image_1)
 
-  find_keypoints(sample)
+  #find_keypoints(sample)
 
-  kp1,des1 = orb.detectAndCompute(image_1,None)
-  sample_w_kp = cv2.drawKeypoints(sample, kp1)
-  cv2.imwrite("kp.jpg", sample_w_kp);
+  #kp1,des1 = orb.detectAndCompute(image_1,None)
+  #sample_w_kp = cv2.drawKeypoints(sample, kp1)
+  #cv2.imwrite("kp.jpg", sample_w_kp);
 
-  bibs = find_bibs(image_1)
+  bib = find_bib(image_1)
   image_1 = cv2.imread("binary.jpg")
 
-  for bib in bibs:
-    x,y,w,h = cv2.boundingRect(bib)
-    #print x,y,w,h
-    cv2.rectangle(image_1,(x,y),(x+w,y+h),(0,255,0),2)
+  x,y,w,h = cv2.boundingRect(bib)
+  cv2.drawContours(image_1,[bib],-1,(0,0,255), 2)
+  cv2.rectangle(image_1,(x,y),(x+w,y+h),(0,255,0),2)
 
   cv2.imwrite("out.jpg", image_1)
 
