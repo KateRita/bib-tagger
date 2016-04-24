cc84dcf5247c91228e1dd68e22e697895f437b05
Kate Reading <katerita@gmail.com>
2016-04-24 15:04:42 -0700

    added pytesseract reference link

--- bibtagger/ocr.py
+++ bibtagger/ocr.py
@@ -3,7 +3,7 @@ from PIL import Image
 from pytesseract import image_to_string
 
 #tesseract command line options: https://tesseract-ocr.googlecode.com/svn/trunk/doc/tesseract.1.html
-#image_to_string implementation/syntax: 
+#image_to_string implementation/syntax: https://github.com/madmaze/pytesseract/blob/master/src/pytesseract.py
 def getOcr(filename):
     #,config='-psm 10') option for single digit recognition
     #return image_to_string(Image.open(filename),config="-psm 6") #5
