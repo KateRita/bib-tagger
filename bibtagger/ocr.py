c98bf6967cde15c2eb14ff9ff11abfb3d527bf8d
Kate Reading <katerita@gmail.com>
2016-04-24 15:03:50 -0700

    added notes on ocr experimentation

--- bibtagger/ocr.py
+++ bibtagger/ocr.py
@@ -2,5 +2,12 @@ from PIL import Image
 
 from pytesseract import image_to_string
 
+#tesseract command line options: https://tesseract-ocr.googlecode.com/svn/trunk/doc/tesseract.1.html
+#image_to_string implementation/syntax: 
 def getOcr(filename):
-    return image_to_string(Image.open(filename))
+    #,config='-psm 10') option for single digit recognition
+    #return image_to_string(Image.open(filename),config="-psm 6") #5
+    #return image_to_string(Image.open(filename)) #3
+    #return image_to_string(Image.open(filename),config="-psm 7") #5
+    return image_to_string(Image.open(filename),config="-psm 7 digits") #4
+
